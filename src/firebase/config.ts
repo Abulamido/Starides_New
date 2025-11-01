3140936a6693ad8ccef0499d6682a86a618475ad
Abubakar Lamido <lamido665@gmail.com>
2025-11-01 03:55:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2143552053-ccbad",
+  "appId": "1:347067189449:web:a72f6e514751c827266518",
+  "apiKey": "AIzaSyDqtHYc1Jnfscx5X9d4vSFhgRLj8O7GpCA",
+  "authDomain": "studio-2143552053-ccbad.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "347067189449"
+};
