24d4947432340253306cc281128b7503d0957894
Abubakar Lamido <lamido665@gmail.com>
2025-11-01 21:54:53 +0000

    this is frustrarating, fix it now

--- src/firebase/provider.tsx
+++ src/firebase/provider.tsx
@@ -157,6 +157,7 @@ export const useFirebaseApp = (): FirebaseApp => {
 type MemoFirebase<T> = T & {__memo?: boolean};
 
 export function useMemoFirebase<T>(factory: () => T, deps: DependencyList): T {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   const memoized = useMemo(factory, deps);
   
   if (memoized && typeof memoized === 'object') {
